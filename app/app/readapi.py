a282660a17f41c9b64bbc49268882bd74a287b12
Nitin Bhatia <nitinbhatia@gmail.com>
2022-07-07 20:47:20 +0530

    patching

--- app/app/readapi.py
+++ app/app/readapi.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import http
+import urllib3
 
 app = Flask(__name__)
 
@@ -11,9 +13,23 @@ def index():
 @app.route('/read',methods=['POST', 'GET'])
 def read():
     print(request)
-    return "hello read api", 200
+    x = dummyFuncForMock()
+    return "hello read api " + x, 200
+
+@app.route('/readnew',methods=['POST', 'GET'])
+def readnew():
+    http_pool = urllib3.poolmanager
+    http_pool.PoolManager().request(method='post',data = [])
+    
+
+    print(request)
+    x = dummyFuncForMock()
+    return "hello read api " + x, 200
 
 
+
+def dummyFuncForMock():
+    return "1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- tests/test_root.py
+++ tests/test_root.py
@@ -12,11 +12,23 @@ def client():
 
     #app.config['TESTING'] = True
     client = readapi.app.test_client()
+    #readapi.app.test_request_context
 
     yield client
 
+@mock.patch('readapi.dummyFuncForMock')
+def mockDummyFunction():
+    return "2"
+
 
 def test_landing_aliases(client):
-    print(client.get("/").data)
-    assert client.get("/").data.decode('UTF-8') == "hello flask"
+    #print(client.get("/read").data)
+    with mock.patch('app.app.readapi.dummyFuncForMock') as mock_method:
+            mock_method.return_value = "2"
+            assert client.get("/read").data.decode('UTF-8') == "hello read api 2"
+            mock_method.assert_called_once()
+
+    
+    
+    
     
